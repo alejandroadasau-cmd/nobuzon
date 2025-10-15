b45eeb68ea2d119eda7250ff6d107e8399eeafde
luis jahaziel <luisjahaz.ag@gmail.com>
2025-10-14 21:04:49 -0600

    Revert "Update React imports for new form handling"
    This reverts commit aa5061587628917539ff5fcdd2ad212c1572362c.

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import GeneracionMasivaHeader from "@/components/GeneracionMasivaHeader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
